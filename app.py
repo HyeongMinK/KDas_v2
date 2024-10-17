4763491b57f15dc3954b9681b281b3298aaeb5ae
HyeongMinK <aalynm@gmail.com>
2024-10-17 23:56:08 +0900

    수정

--- app.py
+++ app.py
@@ -45,7 +45,8 @@ def main():
         if st.session_state['uploaded_file']:
             st.write(st.session_state['uploaded_file'].name)
             st.session_state['df'] =load_data(st.session_state.uploaded_file)
-            st.session_state['df'].replace(' ', 0, inplace=True)
+            st.session_state['df'].iloc[first_idx[0]:, first_idx[1]:].replace(' ', pd.NA, inplace=True)
+            st.session_state['df'].iloc[first_idx[0]:, first_idx[1]:]= st.session_state['df'].iloc[first_idx[0]:, first_idx[1]:].dropna()
             # 문자열이 포함된 위치 찾기
             string_values = find_string_values(st.session_state['df'], first_idx)
             st.write(string_values)
