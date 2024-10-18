e4fa8d2c92f043f8c6ac3bf2f37a3d6f626332b7
HyeongMinK <aalynm@gmail.com>
2024-10-18 09:35:28 +0900

    수정

--- app.py
+++ app.py
@@ -224,12 +224,15 @@ def main():
         # 최종적으로 N+1*N+1 배열을 새로운 DataFrame에 업데이트
         # 새로운 크기로 DataFrame을 초기화합니다.
         new_df = pd.DataFrame(leontief_with_sums)
-        # 새로운 DataFrame을 기존 DataFrame에 업데이트
+
+        # 현재 DataFrame의 크기를 +1씩 늘리기 (NaN으로 초기화)
+        current_df = current_df.reindex(index=range(existing_rows + 1), columns=range(existing_cols + 1))
+
+        # 새로운 DataFrame을 기존 DataFrame의 적절한 위치에 업데이트
         current_df.iloc[2:2 + new_df.shape[0], 2:2 + new_df.shape[1]] = new_df
 
-        # 업데이트된 DataFrame을 세션 상태에 저장
+        # 세션 상태에 업데이트
         st.session_state['df_for_leontief_with_label'] = current_df
-
         threshold_count(st.session_state['df_for_leontief_with_label'].iloc[2:, 2:])
 
         st.subheader('Leontief 과정 matrices')
