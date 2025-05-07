a9598727efda04a199654f2eaa9ad83e49474a45
HyeongMinK <aalynm@gmail.com>
2025-05-07 17:37:01 +0900

    5/7

--- app.py
+++ app.py
@@ -443,7 +443,7 @@ def main():
                 survived_links = np.count_nonzero(N_final)  # 0이 아닌 값 개수 (살아남은 링크 수)
                 link_ratio = survived_links / total_possible_links  # 비율
 
-                st.write(f"적용된 delta: {win_delta_userinput} / N:{N}")
+                st.write(f"적용된 delta: {st.session_state.delta} / N:{N}")
                 st.write(f"남아 있는 링크 수: {survived_links} / 전체 가능 링크 수: {total_possible_links}")
                 st.write(f"남아 있는 링크 비율: {link_ratio:.4f} ({link_ratio * 100:.2f}%)")
 
