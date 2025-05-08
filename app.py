d021e0d1d9ee61e6866b866cfb9f70adbfaaca40
HyeongMinK <aalynm@gmail.com>
2025-05-08 23:04:47 +0900

    5/7

--- app.py
+++ app.py
@@ -119,9 +119,9 @@ def main():
 
         if 'df_editing' not in st.session_state:
             st.session_state['df_editing'] = st.session_state['df'].copy()
+            col = first_idx[1] - number_of_label          # 라벨 열 위치
             dtype_single = st.session_state['df_editing'].iloc[:, col].dtype
             st.write('해당 열 dtype:', dtype_single)
-            col = first_idx[1] - number_of_label          # 라벨 열 위치
             # 1️⃣ int64 → pandas StringDtype 으로 바로 변환
             st.session_state['df_editing'].iloc[:, col] = (
                 st.session_state['df_editing'].iloc[:, col].astype('string')
